a9780586a3e8346a437c73a8ca119765cc4b853f
aleynaa5 <aleynaigacc00@gmail.com>
2024-12-26 23:10:02 +0300

    Add files via upload
    Bilgisayar mühendisliği öğrencilerinin derslerle ilgili sorularını sorabilecekleri, kullanıcı giriş ve üyelik sistemi bulunan bir web sitesi projesi

---
+++ script.js
@@ -0,0 +1,40 @@
+const minimum =1;
+const maksimum =100;
+const answer = (Math.random()* (maksimum - minimum))
+
+console.log(answer);
+
+
+
+
+
+
+
+
+
+
+
+//console.log("Hello");
+
+//window.alert("sa");
+// window.alert("as");
+
+// Comments /* */
+
+//document.getElementById('myP').innerText = "Arda Aslan";   
+//document.getElementById("myP").style.color = "red";
+//document.getElementById("myH1").style.color = "#cccc00";
+
+//let x;
+//x = 100;
+//let myname = "Arda"
+
+//console.log(x);
+//console.log(typeof x);
+//console.log('Benim adım ${x}');
+//console.log($'Benim adim {name}')
+
+//document.getElementById("p1").textContent = myname;
+//document.getElementById("p2").textContent = x;
+//document.getElementById("p3").textContent = myname;
+
